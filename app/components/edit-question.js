366349d60715f7a2ac92a5e42d6da1b40c74d8fc
Brian Fan <kuomu.fan@yahoo.com>
2017-06-02 11:04:13 -0700

    add updateQuestion function for user to update question

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    updateQuestion(question) {
+      var params = {
+        author: this.get('author'),
+        date: this.get('date'),
+        category: this.get('category'),
+        content: this.get('content')
+      };
+      this.sendAction('updateQuestion', question, params);
+    }
+  }
 });
