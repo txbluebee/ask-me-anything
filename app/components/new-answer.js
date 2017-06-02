e86db116a9812656a754611c092731d8ece86dbc
Brian Fan <kuomu.fan@yahoo.com>
2017-06-02 11:27:55 -0700

    add saveAnswer function to let user answer question and also display answers on the same page as the question they respond to

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveAnswer() {
+     var params = {
+       author: this.get('author')? this.get('author') : "",
+       date: this.get('date')? this.get('date') : "",
+       content: this.get('content')? this.get('content') : "",
+       question: this.get('question')
+     };
+     this.sendAction('saveAnswer', params);
+   }
+  }
 });
