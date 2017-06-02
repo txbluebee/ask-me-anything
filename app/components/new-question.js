817410fbfef9f3a0a59cc758618a22b5c6d1556b
Brian Fan <kuomu.fan@yahoo.com>
2017-06-02 10:03:14 -0700

    add saveQuestion function and let user to create new question

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveQuestion() {
+      var params = {
+        author: this.get('author')? this.get('author') : "",
+        date: this.get('date')? this.get('date') : "",
+        category: this.get('category')? this.get('category') : "",
+        content: this.get('content')? this.get('content') : ""
+      };
+      this.sendAction('saveQuestion', params);
+      // clear the form
+      this.set('author', '');
+      this.set('date', '');
+      this.set('category', '');
+      this.set('content', '');
+    }
+  }
 });
